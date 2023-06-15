a05b95278deef7f10e31d7a7d75c83b4db483d91
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2023-06-15 14:28:20 +0100

    force a read-only error by freezing a property.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+import * as Rox from 'rox-browser'
+
+import {
+  useCallback,
+  useEffect,
+  useState,
+} from "react"
+
+const envId = '612f5d0a30c0b442ab612c3c'
+
+const a = 'a'
+console.log('a1=', a)
+a = 'b'
+console.log('a2=', a)
+console.log('Freezing')
+const frozenArray = Object.freeze([0, 1, 2])
+console.log('Modifying')
+frozenArray[0]++ // TypeError
+console.log('Done')
+
+// Create a Roxflag in the flags container class
+const flags = {
+  enableTutorial: new Rox.Flag(),
+  titleColors: new Rox.RoxString('White', ['White', 'Blue', 'Green', 'Yellow']),
+  titleSize: new Rox.RoxNumber(12, [12, 14, 18, 24])
+}
 
 function App() {
+
+  const [values, setValues] = useState({})
+
+  const roxSetup = useCallback(async () => {
+    try {
+      console.log("Initializing SDK")
+      Rox.register(flags)
+      await Rox.setup(envId, {
+        configurationFetchedHandler: (fetcherResult: RoxFetcherResult) => {
+          if(fetcherResult.hasChanges) {
+            console.log('Received new flag values.')
+            // setState({featureFlags: { ...FeatureFlags }})
+            setValues({
+              enableTutorial: flags.enableTutorial.getValue(),
+              titleColors: flags.titleColors.getValue(),
+              titleSize: flags.titleSize.getValue(),
+            })
+          }
+        },
+      })
+      setValues({
+              enableTutorial: flags.enableTutorial.getValue(),
+              titleColors: flags.titleColors.getValue(),
+              titleSize: flags.titleSize.getValue(),
+            })
+      console.log("Rox was initialized")
+
+    } catch (err) {
+      console.error("Rox initialization failed", err)
+    }
+  }, [])
+
+
+  useEffect(() => {
+    roxSetup()
+  }, [roxSetup])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +81,14 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <p>enableTutorial: {values.enableTutorial}</p>
+          <p>titleColors: {values.titleColors}</p>
+          <p>titleSize: {values.titleSize}</p>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
